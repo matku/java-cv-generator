9a8b30161f7d6499ce197290bcebce2219e59b3a
joe.durisin@gmail.com <joe.durisin@gmail.com@1b34dd9f-fa03-283d-9fb1-dfcebd02d024>
2013-05-28 15:36:11 +0000

    - create- RegistrationTest.java - update -XMLWriter.java (create method deleteUser)

---
+++ Jarvan/src/test/java/cz/muni/fi/jarvan/auth/RegistrationTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.jarvan.auth;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests for methods from Registration class 
+ * @author joe
+ */
+public class RegistrationTest {
+    
+    
+    @Test
+    public void testUserExist()
+    {
+        System.out.println("Test 1: User already exists");
+        Registration reg = new Registration("zajac", "testval", "testval");
+        boolean exResult = false;
+        boolean result = reg.tryRegister();
+        
+        assertEquals("username zajac", exResult, result);
+        
+    }
+    
+    
+    @Test
+    public void testEmailExist()
+    {
+        System.out.println("Test 2: Email already exists");
+        Registration reg = new Registration("testval", "zajac@zajac.sk", "testval");
+        boolean exResult = false;
+        boolean result = reg.tryRegister();
+        
+        assertEquals("email zajac@zajac.sk", exResult, result);
+        
+    }
+    
+    @Test
+    public void testNoneExist()
+    {
+        System.out.println("Test 3: None existing values");
+        XMLWriter userXml = new XMLWriter(Settings.getPathUser());
+        Registration reg = new Registration("testval", "testval", "testval");
+        boolean exResult = true;
+        boolean result = reg.tryRegister();
+        userXml.deleteUser("testval");
+        assertEquals("none existing", exResult, result);    
+    }
+
+}
