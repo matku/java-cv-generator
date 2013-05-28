f063b5a2dce9b802185c4484b74568e2978b15bd
mproksik <mproksik@1b34dd9f-fa03-283d-9fb1-dfcebd02d024>
2013-05-28 12:00:10 +0000

    - created library and added contructor for creating library.xml

--- Jarvan/src/main/java/cz/muni/fi/jarvan/auth/Library.java
+++ Jarvan/src/main/java/cz/muni/fi/jarvan/auth/Library.java
@@ -1,7 +1,12 @@
 
 package cz.muni.fi.jarvan.auth;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class for storing CV like database, will parse library.xml
@@ -9,14 +14,36 @@ import java.io.File;
  */
 public class Library {
     
+    private final static Logger log = LoggerFactory.getLogger(Library.class);
+    private String libraryName;
+    
     /**
-     * Checks if library.xml exists, if not it will create file.
+     * Checks if library.xml exists, if not it will create it.
      */
     public Library()
     {
-        if(!(new File(Settings.getPathLibrary()).exists()))
+        
+        this.libraryName = Settings.getPathLibrary();
+        
+        File library = new File(this.libraryName);
+        if(!library.exists())
         {
+            log.info("File library doesn't exist. Creating ...");
             
+            try
+            {
+                library.createNewFile();
+                FileWriter fw = new FileWriter(library);    
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
+                bw.write("<library>");
+                bw.write("</library>");
+                bw.close();
+            }
+            catch (IOException e)
+            {
+                log.error("File library.xml not created" + e.getMessage());
+            }
         }
     }
 }
